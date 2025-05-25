5350724dd8a5420d680905988a283b2c5d150867
TakaXTakaX <swanyeeht@gmail.com>
2025-05-26 00:58:09 +0700

    Taka BE tickets backend all working

--- Backend/src/routes/index.route.ts
+++ Backend/src/routes/index.route.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import { userRouter } from "./user.route.ts";
+import { ticketRouter } from "./ticket.route.ts";
 const mainRouter = new Hono();
 
 mainRouter.route("/users", userRouter);
+mainRouter.route("/tickets", ticketRouter);
 export { mainRouter };
